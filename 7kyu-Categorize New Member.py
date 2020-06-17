69eba77ef8bdc382ada2857d92bfe36398d90549
Wan-Yu Liao <39834267+WanYuLiao@users.noreply.github.com>
2020-06-17 15:54:26 +0800

    Update 7kyu-Categorize New Member.py

--- 7kyu-Categorize New Member.py
+++ 7kyu-Categorize New Member.py
@@ -1,5 +1,5 @@
 """
-Categorize New Member Ver.1
+Categorize New Member 
 7kyu
 
 The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
